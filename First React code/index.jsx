4f61e88f7b75eaa5b10b342ce3fbf5618f27f0f3
Butenkite <butenkite@gmail.com>
2025-10-04 13:16:03 -0700

    lesson 1

---
+++ First React code/index.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+
+//create a root
+const root = createRoot(document.getElementById("root"));
+// render some markup
+root.render(<p>Testing Testing</p>);
